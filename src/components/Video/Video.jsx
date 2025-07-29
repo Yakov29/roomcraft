9d94a101e5f6a7dd01bb7393153f3d6f5610d4b0
Yakov <dirusya1ya@gmail.com>
2025-07-29 23:04:21 +0300

    видео на последок) 🔥

---
+++ src/components/Video/Video.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from "react";
+import Container from "../Container/Container";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import "./Video.css";
+
+const VideoPlayer = () => {
+    useEffect(() => {
+        AOS.init({
+            duration: 800,
+            once: true,
+        });
+    }, []);
+
+    return (
+        <section className="video-player" data-aos="fade-up">
+            <Container>
+                <h2 className="video-player__title" data-aos="fade-right">
+                    Нове відео туторіал 📹
+                </h2>
+                <div className="video-player__wrapper" data-aos="zoom-in">
+                    <div className="video-player__iframe-wrapper">
+                        <iframe
+                            className="video-player__iframe"
+                            src="https://www.youtube.com/embed/78z3i8WNVEA"
+                            title="RoomCraft туторіал"
+                            frameBorder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen
+                        ></iframe>
+                    </div>
+                </div>
+            </Container>
+        </section>
+    );
+};
+
+export default VideoPlayer;
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -3,6 +3,7 @@ import Hero from "../../components/Hero/Hero";
 import About from "../../components/About/About";
 import Footer from "../../components/Footer/Footer";
 import Developer from "../../components/Developer/Developer";
+import Video from "../../components/Video/Video";
 
 const Homepage = () => {
     useEffect(() => { document.title = "RoomCraft | Головна" })
@@ -10,6 +11,7 @@ const Homepage = () => {
         <main>
             <Hero />
             <About/>
+            <Video/>
             <Developer/>
             <Footer/>
         </main>
