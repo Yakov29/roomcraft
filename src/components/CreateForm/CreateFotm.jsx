f70b56b2d6de82ece06d102b23098b45a590184a
Yakov <dirusya1ya@gmail.com>
2025-08-01 15:27:23 +0300

    Защита 🔥

--- src/components/CreateForm/CreateFotm.jsx
+++ src/components/CreateForm/CreateFotm.jsx
@@ -5,7 +5,7 @@ import "./CreateForm.css";
 
 const CreateForm = () => {
   const navigate = useNavigate();
-  const user = JSON.parse(localStorage.getItem("user")) || { rooms: [] };
+  const user = JSON.parse(localStorage.getItem("user")) || { rooms: [], id: Date.now() };
 
   const [roomName, setRoomName] = useState("");
   const [roomType, setRoomType] = useState("");
@@ -27,35 +27,36 @@ const CreateForm = () => {
     };
 
     localStorage.setItem("user", JSON.stringify(updatedUser));
+
     navigate(`/user/${updatedUser.id}/rooms`);
   };
 
   return (
-    <section className="createform">
-      <Container>
-        <h2 className="createform__title">Зробіть все на свій смак і насолоджуйтесь!</h2>
-        <form className="createform__form" onSubmit={handleSubmit}>
-          <ul className="createform__list">
-            <li className="createform__item">
-              <p className="createform__name">Назва кімнати</p>
-              <input
-                className="createform__input"
-                type="text"
-                placeholder={`Введіть назву кімнати`}
-                value={roomName}
-                onChange={(e) => setRoomName(e.target.value)}
-              />
-            </li>
-          </ul>
-          <p className="createform__warn">
-            Зверніть увагу: редактор працює на технології WebGL — переконайтеся, що ваш пристрій її підтримує.
-          </p>
-          <button type="submit" className="createform__submit">
-            Створити кімнату
-          </button>
-        </form>
-      </Container>
-    </section>
+      <section className="createform">
+        <Container>
+          <h2 className="createform__title">Зробіть все на свій смак і насолоджуйтесь!</h2>
+          <form className="createform__form" onSubmit={handleSubmit}>
+            <ul className="createform__list">
+              <li className="createform__item">
+                <p className="createform__name">Назва кімнати</p>
+                <input
+                    className="createform__input"
+                    type="text"
+                    placeholder={`Введіть назву кімнати`}
+                    value={roomName}
+                    onChange={(e) => setRoomName(e.target.value)}
+                />
+              </li>
+            </ul>
+            <p className="createform__warn">
+              Зверніть увагу: редактор працює на технології WebGL — переконайтеся, що ваш пристрій її підтримує.
+            </p>
+            <button type="submit" className="createform__submit">
+              Створити кімнату
+            </button>
+          </form>
+        </Container>
+      </section>
   );
 };
 
