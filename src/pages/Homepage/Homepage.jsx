9c512b57711bfac7ae5ace553416c6604efe38c1
Yakov <dirusya1ya@gmail.com>
2025-08-04 23:58:25 +0300

    🥈

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -4,12 +4,14 @@ import About from "../../components/About/About";
 import Footer from "../../components/Footer/Footer";
 import Developer from "../../components/Developer/Developer";
 import Video from "../../components/Video/Video";
+import Winner from "../../components/Winner/Winner";
 
 const Homepage = () => {
     useEffect(() => { document.title = "RoomCraft | Головна" })
     return (
         <main>
             <Hero />
+            <Winner/>
             <About/>
             <Video/>
             <Developer/>
